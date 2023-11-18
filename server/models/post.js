beaf259db87c757999ae9ef820e0bb1ffd5bd749
priyakumi <135395465+priyakumi@users.noreply.github.com>
2023-11-17 20:43:09 -0800

    intial push for server

---
+++ server/models/post.js
@@ -0,0 +1,40 @@
+
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+    postText: {
+      type: String
+    },
+    datePosted: {
+      type:String
+    },
+    timeLimit: {
+      type: Number
+    },
+    likes: {
+      type: Number
+    } 
+  
+});
+const Post = mongoose.model('Post', postSchema);
+
+module.exports = Post;
+/*postText: {
+  type: String,
+  maxlength: 255,
+  required: true,
+},
+datePosted: {
+  type: Date,
+  default: Date.now,
+},
+timeLimit: {
+  type: Number,
+  default: 24 * 60 * 60 * 1000, // Default to 1 day in milliseconds
+},
+likes: {
+  type: Number,
+  default: 0,
+}*/
---
+++ server/models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+//const post = require('./post');
+
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    maxlength: 64,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    set: async function (password) {
+        const saltRounds = 10;
+      return await bcrypt.hash(password, saltRounds);
+    },
+  },
+  //posts: [post],
+  //likedPosts: [Schema.Types.ObjectId],
+
+  
+});
+
+// Need a method that sees if a password is correct (use bcrypt compare)
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
