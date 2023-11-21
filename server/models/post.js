a7ef06265b84ff261802777943c23df3fed53320
priyakumi <135395465+priyakumi@users.noreply.github.com>
2023-11-20 19:15:39 -0800

    seeds, query, model,login and adduser code added

--- server/models/post.js
+++ server/models/post.js
@@ -4,18 +4,23 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const postSchema = new Schema({
-    postText: {
-      type: String
-    },
-    datePosted: {
-      type:String
-    },
-    timeLimit: {
-      type: Number
-    },
-    likes: {
-      type: Number
-    } 
+  postText: {
+    type: String,
+    maxlength: 255,
+    required: true,
+  },
+  datePosted: {
+    type: Date,
+    default: Date.now,
+  },
+  timeLimit: {
+    type: Number,
+    default: 24 * 60 * 60 * 1000, // Default to 1 day in milliseconds
+  },
+  likes: {
+    type: Number,
+    default: 0,
+  }
   
 });
 const Post = mongoose.model('Post', postSchema);
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,25 +1,42 @@
+const { gql } = require('apollo-server-express');
 
+const typeDefs = gql`
+  type Post {
+    _id: ID
+    postText: String
+    datePosted: Int
+    timeLimit: Int
+    likes: Int
+  }
 
-const typeDefs = `
-type User {
-  _id: ID
-  username: String
-  password: String
-}
+  type User {
+    _id: ID
+    username: String
+    password: String
+    posts: [Post]
+    likedPosts: [Post]
+  }
 
-type Post {
-  _id: ID
-  postText: String
-  datePosted: String
-  timeLimit: Int
-  likes: Int
-}
-  
+  type Auth {
+    token: ID!
+    user: User
+  }
+
   type Query {
     getUser: [User]
+    getUserById(_id: ID): User
     getPost: [Post]
+    getPostById(_id: ID): Post
+    me: User
+  }
+
+  type Mutation {
+    
+    login(username: String!, password: String!): Auth
+    addUser(username: String!, password: String!): Auth
   }
 `;
 
-
 module.exports = typeDefs;
+//addUser(username: String, password: String): User
+    //addPost(postText: String): Post
